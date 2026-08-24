5228d3a5b238d57e3c94445bbc88909c56e1176a
Kylie Sy <10233535+kksy@users.noreply.github.com>
2026-08-24 16:00:41 +1000

    add ability to create root folder

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,44 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import App from './App'
 
 describe('App', () => {
-  it('displays Hello World', () => {
+  it('should not show form at the start', async () => {
     render(<App />)
-    expect(screen.getByText('hello world')).toBeInTheDocument()
+
+    expect(screen.queryByRole('form', { name: /add folder/i })).not.toBeInTheDocument()
+  })
+
+  it('should create a folder with name when confirm button is clicked', async () => {
+    const user = userEvent.setup()
+    render(<App />)
+    await user.click(screen.getByRole('button', { name: /add folder to root/i }))
+
+    expect(screen.getByRole('form', { name: /add folder/i })).toBeInTheDocument()
+
+    const expectedFolderName = 'new folder'
+    const input = screen.getByRole('textbox', { name: /name/i })
+
+    await user.click(input)
+    await user.keyboard(expectedFolderName)
+    await user.click(screen.getByRole('button', { name: /confirm/i }))
+
+    expect(screen.getByText(expectedFolderName)).toBeInTheDocument()
+  })
+
+  it('should not create a folder when button is clicked and no name is set', async () => {
+    const user = userEvent.setup()
+    render(<App />)
+    await user.click(screen.getByRole('button', { name: /add folder to root/i }))
+
+    expect(screen.getByRole('form', { name: /add folder/i })).toBeInTheDocument()
+
+    const input = screen.getByRole('textbox', { name: /name/i })
+
+    await user.click(input)
+    await user.click(screen.getByRole('button', { name: /confirm/i }))
+
+    expect(screen.queryByRole('listitem')).not.toBeInTheDocument()
   })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
+import { useState, type SubmitEvent } from 'react';
+import FolderOpenRegularIcon from './icons/folder-open-regular.svg';
 import './App.css';
 
+type Node = {
+  type: 'folder' | 'file' | 'unset';
+  name?: string;
+  children?: Node[];
+  id: string;
+}
+
 function App() {
+  const [showForm, setShowForm] = useState(false)
+  const [nodes, setNodes] = useState<Node[]>([])
+  
+  function handleAddFolderToRootClick() {
+    setShowForm(true)
+  }
+
+  function handleFormSubmit(event: SubmitEvent) {
+    event.preventDefault()
+    const formData = new FormData(event.target);
+    const inputValue = formData.get('name')
+    if(inputValue) {
+      setNodes([...nodes, {
+      id: crypto.randomUUID(),
+      type: 'folder',
+      name: inputValue.toString(),
+    }])
+    }
+    
+  }
+
   return (
     <main>
       <h1 className="Heading">Folder Structure Maker</h1>
+      <button onClick={handleAddFolderToRootClick}>Add folder to root</button>
+      {showForm && (<form aria-label="Add folder" onSubmit={handleFormSubmit} className="node-form">
+        <label className="input-with-icon">
+          <img className="Form__icon" src={FolderOpenRegularIcon} alt="" />
+          <span className="visually-hidden">Folder name</span>
+          <input aria-label="Folder name" name="name" />
+        </label>
+        <button type="submit" aria-label="confirm">✓</button>
+        <button aria-label="cancel" type="button">x</button>
+      </form>)}
+      <ul>
+        {nodes.map((node) => {
+          return (
+            <li id={node.id}>{node.name}</li>
+          )
+        })}
+      </ul>
     </main>
   )
 }
